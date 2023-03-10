9a667d24aa1aca423198796fd579569bd34f109b
rxruiz <101907828+rxruiz@users.noreply.github.com>
2023-03-10 16:10:38 -0600

    Create mod8_rxrfcb.py

---
+++ mod8_rxrfcb.py
@@ -0,0 +1,12 @@
+def fizz_buzz():
+  for x in range(1,101):
+    if x%5 == 0 and x%3 == 0:
+      print("FizzBuzz")
+    elif x%5 == 0:
+      print("Buzz")
+    elif x%3 == 0:
+      print("Fizz")
+    else:
+      print(x)
+      
+      
